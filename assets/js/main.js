88332ab154e5e297b3c91250270fefc08a4f8171
Dyachuk-P <74014985+Dyachuk-P@users.noreply.github.com>
2021-03-19 12:22:13 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,21 @@ $('#fon').vide({
 });
 
 } );
+
+// function elem(){
+//   var elem = document.querySelector("#mailing");
+//   elem.classList.add("none");
+
+// }
+
+
+// function 
+// var nice = document.querySelector("#p-nn")
+
+
+
+
+//   elem.classList.add("block")
+// var elem = document.querySelector("#mailing");
+// elem.classList.add("none");
+// alert(elem.classList)
